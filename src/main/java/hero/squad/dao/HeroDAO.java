60ddf16ac6aa4e3b20b8fae3bcbb365ce6656fe5
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-08-09 01:51:43 +0300

    changes made to herosquad

---
+++ src/main/java/hero/squad/dao/HeroDAO.java
@@ -0,0 +1,19 @@
+package hero.squad.dao;
+import java.util.List;
+import hero.squad.models.Hero;
+
+public interface HeroDAO {
+    List<Hero> getAll();
+    // CREATE
+    void add(Hero hero);
+
+    void update(Hero hero);
+
+    // READ
+    Hero findById(int id);
+
+    //DELETE
+    void deleteById(int id);
+
+    List<Hero> getHeroesBySquad(int squadId);
+}
